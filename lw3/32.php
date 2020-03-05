9998066c1eb6c9e0d88a70472a9f531fe10f5e43
tau0vsegda <tau13613@gmail.com>
2020-03-05 17:21:44 +0300

    commit 1

--- lw3/32.php
+++ lw3/32.php
@@ -1,2 +1,18 @@
 <?php
 
+header("Content-Type: text/plain");
+function getGETParameter(string $name): ?string
+{
+    return isset($_GET[$name]) ? (string) $_GET[$name] : null;
+}
+$identifier = getGETParameter('identifier');
+if ($identifier === null)
+{
+    echo 'not found GET parameter';
+}
+else
+{
+    echo $identifier;
+    echo "\n";
+    echo preg_match("/^[A-Za-z]+[A-Za-z0-9]*$/", $identifier) ? "Yes" : "No";
+}
